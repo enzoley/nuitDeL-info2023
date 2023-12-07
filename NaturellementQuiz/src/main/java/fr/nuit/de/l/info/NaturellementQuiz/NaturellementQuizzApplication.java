0319b108cf97bf6e2b4f84d20ca4a12c240dcd7e
Nathan <nchretien@et.esiea.fr>
2023-12-07 21:01:40 +0100

    ta mère lucas

---
+++ NaturellementQuiz/src/main/java/fr/nuit/de/l/info/NaturellementQuiz/NaturellementQuizzApplication.java
@@ -0,0 +1,13 @@
+package fr.nuit.de.l.info.NaturellementQuiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaturellementQuizzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaturellementQuizzApplication.class, args);
+	}
+
+}
---
+++ NaturellementQuiz/src/test/java/fr/nuit/de/l/info/NaturellementQuiz/NaturellementQuizzApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.nuit.de.l.info.NaturellementQuiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaturellementQuizzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
