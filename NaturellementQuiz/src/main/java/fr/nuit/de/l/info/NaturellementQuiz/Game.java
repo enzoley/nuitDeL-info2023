583ab77d29d01702bbaef5912a1c67a092b61fd1
Nathan <nchretien@et.esiea.fr>
2023-12-08 04:29:08 +0100

    controllers

--- NaturellementQuiz/src/main/java/fr/nuit/de/l/info/NaturellementQuiz/Game.java
+++ NaturellementQuiz/src/main/java/fr/nuit/de/l/info/NaturellementQuiz/Game.java
@@ -1,8 +1,15 @@
 package fr.nuit.de.l.info.NaturellementQuiz;
 
+import fr.nuit.de.l.info.NaturellementQuiz.model.User;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
 public class Game {
 
-    public static Set<Integer> listIdQuestion ;
+    public static List<Long> listIdQuestion  = new ArrayList<>();
+    public static User player;
+
+
 }
--- NaturellementQuiz/src/main/java/fr/nuit/de/l/info/NaturellementQuiz/controller/game.java
+++ NaturellementQuiz/src/main/java/fr/nuit/de/l/info/NaturellementQuiz/controller/game.java
@@ -1,10 +1,13 @@
 package fr.nuit.de.l.info.NaturellementQuiz.controller;
 
+import fr.nuit.de.l.info.NaturellementQuiz.Game;
 import fr.nuit.de.l.info.NaturellementQuiz.repo.QuestionRepo;
+import fr.nuit.de.l.info.NaturellementQuiz.repo.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -13,20 +16,37 @@ public class game {
     @Autowired
     private QuestionRepo questionRepo;
 
-    @GetMapping("/game/{round}")
-    public ModelAndView gameStart(@PathVariable(value = "round") int round){
-        ModelAndView gameModel = new ModelAndView("game.html");
-        int size = questionRepo.findAll().size();
-        Long random = (long) (Math.random()*size);
-        if(questionRepo.findById(random).isPresent()){
-            String erreur = "";
-            gameModel.addObject("question",questionRepo.findById(random).get());
-            gameModel.addObject("erreur",erreur);
+    @Autowired
+    private UserRepo userRepo;
 
-        }else{
+    @GetMapping("/game/{round}")
+    public ModelAndView gameStart(@PathVariable(value = "round") int round) {
+        ModelAndView gameModel = new ModelAndView("game.html");
+        gameModel.addObject("user",Game.player );
+
+        int size = questionRepo.findAll().size();
+        Long random = getRandom(size);
+        if (questionRepo.findById(random).isPresent()) {
+            String erreur = "";
+            gameModel.addObject("question", questionRepo.findById(random).get());
+            gameModel.addObject("erreur", erreur);
+
+        } else {
             String erreur = "Une erreur est survenue";
-            gameModel.addObject("erreur",erreur);
+            gameModel.addObject("erreur", erreur);
         }
+
+
         return gameModel;
+    }
+
+    private Long getRandom(int size) {
+        Long random = (long) (Math.random() * size);
+        if (Game.listIdQuestion.contains(random)) {
+            return getRandom(size);
+        } else {
+            Game.listIdQuestion.add(random);
+            return random;
+        }
     }
 }
